139d799377ba77d06fc7a5b05c7bbc8a081b0243
Daniel Ramos Mendoza <heber.daniel.ramos.mendoza@gmail.com>
2020-10-12 18:13:02 -0500

    Segunda versión de búsqueda de proyectos por ambito

---
+++ WidgetProyectos2/nls/strings.js
@@ -0,0 +1,9 @@
+define({
+  root: {
+    _widgetLabel: 'Widget Proyectos2',
+    widgetTitle: 'Widget Proyectos2',
+    description: 'A custom Web AppBuilder widget.'
+  }
+  // add supported locales below:
+  // , "zh-cn": true
+});

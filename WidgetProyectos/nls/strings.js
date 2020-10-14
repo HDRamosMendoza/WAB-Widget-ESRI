9d22082c5e818a6678b4fc65e1c3d9e2808d7f4b
Daniel Ramos Mendoza <heber.daniel.ramos.mendoza@gmail.com>
2020-10-14 16:23:30 -0500

    Se agrega lenguaje

---
+++ WidgetProyectos/nls/strings.js
@@ -0,0 +1,9 @@
+define({
+  root: {
+    _widgetLabel: 'Widget Proyectos',
+    widgetTitle: 'Widget Proyectos',
+    description: 'A custom Web AppBuilder widget.'
+  }
+  // add supported locales below:
+  // , "zh-cn": true
+});
